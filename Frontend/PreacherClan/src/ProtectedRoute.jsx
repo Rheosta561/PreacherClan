c83bce1bea12f2d76a29dd9749c16d1b0bcdf3b1
Anubhav Mishra <manubhav731@gmail.com>
2025-06-18 16:04:07 +0530

    protected route fixed

--- Frontend/PreacherClan/src/ProtectedRoute.jsx
+++ Frontend/PreacherClan/src/ProtectedRoute.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const user = localStorage.getItem('user');
+  const user = JSON.parse(localStorage.getItem('user'));
   const token = localStorage.getItem('token');
 
-  if (!user && !token) {
+  if (!user || !token) {
     return <Navigate to="/login" replace />;
   }
 
