fd6811428f05ddd7209f7032a7e897d6ae3d659b
Anubhav Mishra <manubhav731@gmail.com>
2026-02-06 03:58:33 +0530

    domain config added

--- Backend/Utils/emailService.js
+++ Backend/Utils/emailService.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
-      from: "Preacher Clan <no-reply@preacherclan.com>",
+      from: "Preacher Clan <no-reply@preacherclan.in>",
       to,
       subject,
       html,
